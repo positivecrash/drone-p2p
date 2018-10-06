f6a2e1c0eaa42b7803a8316106e3ed6cfbe04e0e
Anastasiia Bakai <anastasiia.bakai@gmail.com>
2018-10-06 23:22:51 +0400

    new pages

---
+++ dist/assets/js/main.js
@@ -0,0 +1,112 @@
+window.onload=function(){
+
+	/* BASIC FUNCTIONS */
+
+	function getCookie(name) {
+	  var matches = document.cookie.match(new RegExp(
+	    "(?:^|; )" + name.replace(/([\.$?*|{}\(\)\[\]\\\/\+^])/g, '\\$1') + "=([^;]*)"
+	  ));
+	  return matches ? decodeURIComponent(matches[1]) : undefined;
+	}
+
+	/* end of BASIC FUNCTIONS */
+
+
+
+	/* TOGGLE THEME */
+	var themeToggler = document.querySelector('#js-change-theme');
+	var themeLight = 'theme-light';
+	var themeDark = 'theme-dark';
+
+	function switchTheme(theme){
+
+		if (theme){
+			document.body.classList.remove(themeLight, themeDark);
+			document.body.classList.add(theme);
+			themeToggler.dataset.theme = theme;
+			document.cookie = "theme="+theme;
+		}
+		
+	}
+
+
+	if(themeToggler){
+
+		var cookieTheme = getCookie('theme');
+
+		if(cookieTheme){
+			switchTheme(cookieTheme);
+		}
+		else{
+			switchTheme(themeDark);
+		}
+
+		themeToggler.addEventListener('click', function(){
+
+			if(this.dataset.theme == themeDark){
+				this.dataset.theme = themeLight;
+				switchTheme(themeLight);
+			}
+			else{
+				this.dataset.theme = themeDark;
+				switchTheme(themeDark);
+			}
+		});
+	}
+	/* end of TOGGLE THEME */
+
+
+
+
+	/* TEXT SIZE CHANGE */
+
+	var globalTextSize = 100;
+
+	function setTextSize(size){
+		document.documentElement.style.fontSize = size + '%';
+		document.querySelector('#js-text-size-label').innerHTML = size + '%';
+		document.cookie = "textsize="+size;
+	}
+
+	var textSize = getCookie('textsize');
+
+	if(textSize){
+		setTextSize(textSize);
+		globalTextSize = textSize;
+	}
+
+	document.querySelector('#js-text-size-minus').addEventListener('click', function(){
+		globalTextSize = parseInt(globalTextSize) - 10;
+		setTextSize(globalTextSize);
+	});
+
+	document.querySelector('#js-text-size-plus').addEventListener('click', function(){
+		globalTextSize = parseInt(globalTextSize) + 10;
+		setTextSize(globalTextSize);
+	});
+
+	/* end of TEXT SIZE CHANGE */
+
+
+
+	/* COOKIE POLICY */
+
+	var policyMsg = document.querySelector('#js-cookiePolicy');
+	var policyBtn = document.querySelector('#js-cookiePolicy .btn-line');
+
+	var cookiePolicy = getCookie('policy');
+	console.log(cookiePolicy);
+
+	if(cookiePolicy){
+		policyMsg.style.display = "none";
+	}
+
+	policyBtn.addEventListener('click', function(){
+		document.cookie = "policy=true";
+		policyMsg.style.display = "none";
+	});
+
+	/* end of COOKIE POLICY */
+
+
+}
---
+++ gulpfile.js
@@ -0,0 +1,120 @@
+//load plugins
+var gulp             = require('gulp'),
+    compass          = require('gulp-compass'),
+    pug              = require('gulp-pug'),
+    autoprefixer     = require('gulp-autoprefixer'),
+    cleancss         = require('gulp-clean-css'),
+    uglify           = require('gulp-uglify'),
+    merge            = require('merge-stream'),
+    rename           = require('gulp-rename'),
+    concat           = require('gulp-concat'),
+
+    image            = require('gulp-image'),
+    
+    ttf2eot          = require('gulp-ttf2eot'),
+    ttf2woff         = require('gulp-ttf2woff'),
+
+    iconfont         = require('gulp-iconfont'),
+    iconfontCss      = require('gulp-iconfont-css')
+
+    browserSync      = require('browser-sync');
+
+var fontName = 'CustomIcons';
+
+
+//styles
+gulp.task('styles', function() {
+	return gulp.src(['app/styles/*.scss'])
+		.pipe(compass({
+			// project: 'dist/assets',
+			css: 'dist/assets/css',
+			sass: 'app/styles/',
+			image: 'dist/assets/i',
+			font: 'dist/assets/fonts'
+		}))
+		.pipe(autoprefixer('last 2 version', 'safari 5', 'ie 7', 'ie 8', 'ie 9', 'opera 12.1', 'ios 6', 'android 4'))
+		.pipe(gulp.dest('dist/assets/css'))
+		.pipe(rename({ suffix: '.min' }))
+		.pipe(cleancss({
+          compatibility: 'ie8'
+        }))
+		.pipe(gulp.dest('dist/assets/css'))
+    .pipe(browserSync.reload({stream:true}));
+});
+
+//scripts
+gulp.task('scripts', function() {
+	return gulp.src(['app/scripts/plugins/*.js', 'app/scripts/app.js'])
+		.pipe(concat('main.js'))
+		.pipe(gulp.dest('dist/assets/js'))
+		.pipe(rename({ suffix: '.min' }))
+		.pipe(uglify())
+		.pipe(gulp.dest('dist/assets/js'))
+    .pipe(browserSync.reload({stream:true}));
+});
+
+//templates
+gulp.task('templates', function() {
+  return gulp.src('app/pages/*.pug')
+    .pipe(pug({
+      pretty: true
+    }))
+    .pipe(gulp.dest('dist/'))
+    .pipe(browserSync.reload({stream:true}));
+});
+
+//iconfonts
+gulp.task('iconfont', function(){
+  gulp.src(['app/fonticons/*.svg'])
+    .pipe(iconfontCss({
+      fontName: fontName,
+      fontPath: '../fonts/',
+      path: 'app/styles/templates/font-icons-template.scss',
+      targetPath: '../../../app/styles/utilities/_icons.scss'
+    }))
+    .pipe(iconfont({
+      fontName: fontName,
+      normalize: true,
+      prependUnicode: true,
+      fontHeight: 1001,
+      // timestamp: runTimestamp,
+      // appendCodepoints: true,
+      formats: ['svg', 'ttf', 'eot', 'woff']
+     }))
+    .pipe(gulp.dest('dist/assets/fonts'))
+    .pipe(browserSync.reload({stream:true}));
+});
+
+gulp.task('browserSync', function() {
+  browserSync({
+    server: {
+      baseDir: "dist"
+    },
+    port: 8080,
+    open: true,
+    notify: false
+  });
+});
+
+
+// Watch
+gulp.task('watch', function() {
+
+	//watch .scss files
+	gulp.watch(['app/styles/utilities/*.scss', 'app/styles/sections/*.scss', 'app/styles/pages/*.scss', 'app/styles/plugins/*.scss', 'app/styles/templates/*.scss', 'app/styles/dronep2p.scss'], ['styles']);
+
+	//watch .js files
+	gulp.watch('app/scripts/**/*.js', ['scripts']);
+
+	//watch .jade files
+	gulp.watch('app/pages/**/*.pug', ['templates']);
+
+  //watch font icon files
+  gulp.watch('app/fonticons/*.svg', ['iconfont']);
+
+
+});
+
+
+//default
+gulp.task('default', ['scripts', 'styles', 'templates', 'iconfont', 'watch', 'browserSync']);
